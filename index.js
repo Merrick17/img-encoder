9563d19bc8e81bc22e660fa6fe6f777c6d5e19dd
safwen ben fredj <safwen.benfredj@gmail.com>
2023-03-13 00:54:21 +0100

    sending path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
 const unlinkFile = promisify(fs.unlink);
 const sharp = require("sharp");
 
@@ -23,28 +24,16 @@ const upload = multer({ storage: storage });
 
 app.post("/upload", upload.single("image"), async (req, res) => {
   try {
-    const imagePath = req.file.path;
-    const imageStream = fs.createReadStream(imagePath);
-    const imageResizer = sharp(); 
-    const compressedImageStream = imageStream.pipe(imageResizer);
-    const chunks = [];
-
-    compressedImageStream.on("data", (chunk) => {
-      chunks.push(chunk);
-    });
-
-    compressedImageStream.on("end", async () => {
-      const compressedImageBuffer = Buffer.concat(chunks);
-      const base64Image = compressedImageBuffer.toString("base64");
-      await unlinkFile(imagePath);
-      res.json({ img: base64Image });
-    });
+    const imageBuffer = await sharp(req.file.path).resize({ width:800 }).toBuffer();
+    const base64Image = imageBuffer.toString("base64");
+    //await unlinkFile(req.file.path);
+    res.json({ img: base64Image, url: req.file.path });
   } catch (err) {
     console.error(err);
     res.status(500).send("An error occurred");
   }
 });
-
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
